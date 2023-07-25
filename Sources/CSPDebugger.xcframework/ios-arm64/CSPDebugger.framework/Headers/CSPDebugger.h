382974c6f3989a3e223e233d6da87ac67175623d
Osman T <otufekci@bimser.com.tr>
2023-07-25 14:52:01 +0300

    Reset

---
+++ Sources/CSPDebugger.xcframework/ios-arm64/CSPDebugger.framework/Headers/CSPDebugger.h
@@ -0,0 +1,18 @@
+//
+//  CSPDebugger.h
+//  CSPDebugger
+//
+//  Created by Osman Tufekci on 24.07.2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CSPDebugger.
+FOUNDATION_EXPORT double CSPDebuggerVersionNumber;
+
+//! Project version string for CSPDebugger.
+FOUNDATION_EXPORT const unsigned char CSPDebuggerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CSPDebugger/PublicHeader.h>
+
+
